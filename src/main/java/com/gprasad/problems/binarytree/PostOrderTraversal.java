6c3b0690d68133f1d47ddaaa0f9c738f3daaac51
Guru Prasad <guruprasad.kb@gmail.com>
2021-05-01 10:00:44 -0700

    assignments

---
+++ src/main/java/com/gprasad/problems/binarytree/PostOrderTraversal.java
@@ -0,0 +1,33 @@
+package com.gprasad.problems.binarytree;
+
+//Given a binary tree, return the postorder traversal of its nodes' values.
+// Implement this both recursively and iteratively
+//        Example:
+//
+//        Input: [1,null,2,3]
+//             1
+//              \
+//              2
+//             /
+//            3
+//
+//        Output: [3,2,1]
+import com.gprasad.problems.models.TreeNode;
+
+import java.util.*;
+
+public class PostOrderTraversal implements BinaryTreeDFSTraversal{
+    private List<Integer> output = new ArrayList<>();
+
+    public List<Integer> recursiveTraversal(TreeNode root) {
+        //implement your code here
+        return null;
+    }
+
+
+    public List<Integer> iterativeTraversal(TreeNode root) {
+
+        //implement your code here
+        return null;
+    }
+}
